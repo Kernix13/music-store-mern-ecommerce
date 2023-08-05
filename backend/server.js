bfd099632f94a050755d381321dda38f6db27696
Jim <jimkernicky@gmail.com>
2023-08-05 12:49:03 -0400

    server.js adds made

--- backend/server.js
+++ backend/server.js
@@ -3,31 +3,27 @@ import dotenv from 'dotenv'
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 connectDB();
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('API is running on port 5000...');
 })
 
-// All products route
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-// Single product route
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
 // Start server
-const port = process.env || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`))
